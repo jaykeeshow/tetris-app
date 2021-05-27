87ff7abe46dbf87e56298c565134598a97b3742f
Jayakishore Rajkumar <jkshr.r@gmail.com>
2021-05-27 16:44:32 +0200

    first commit

---
+++ src/components/Cell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
+
+const Cell=({type})=>{
+return(
+    <StyledCell type={type} color={TETROMINOS[type].color}>
+        {console.log('rerender')}
+    </StyledCell>
+)
+};
+export default React.memo(Cell);
---
+++ src/components/StartButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledStartButton } from './styles/StyledStartButton';
+const StartButton=({callback})=>{
+return(
+    <StyledStartButton onClick={callback}>
+        Start game
+    </StyledStartButton>
+)
+};
+export default StartButton;
